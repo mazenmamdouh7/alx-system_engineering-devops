0a039094e4c8a05830075b96b43495956127f7ee
root <mazenmamdouh7>
2024-07-20 21:41:54 -0700

    management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,14 @@
 #!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# Puppet manifest to install Flask version 2.1.0 using pip3
+
+# Ensure pip3 is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install Flask version 2.1.0 using pip3
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+  path    => ['/usr/bin', '/usr/local/bin'],
 }
