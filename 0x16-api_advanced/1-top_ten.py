92e52b0282f853c6e00a277811bdd2774f7f8b4e
root <mazenmamdouh7>
2024-07-22 08:44:17 -0700

    api

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,13 +4,15 @@ import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the first 10 hot posts listed for a given subreddit."""
+    """Print the titles of the first
+    10 hot posts listed for a given subreddit.
+    """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
     params = {'limit': 10}
 
     response = requests.get(url, headers=headers, params=params)
-    
+
     if response.status_code != 200:
         print(None)
         return
